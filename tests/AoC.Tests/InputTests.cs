5e7ae3a05cc0665a9573822d0b662cfc62b171bd
mazharenko <mazharenko.a@yandex.ru>
2024-12-01 12:45:49 +0500

    day 1

--- tests/AoC.Tests/InputTests.cs
+++ tests/AoC.Tests/InputTests.cs
@@ -8,6 +8,7 @@ internal partial class InputTests
 {
     private static IEnumerable<PartInputCaseData> GetCases()
     {
-        yield break;
+	    yield return new PartInputCaseData(1, 1, "1889772");
+	    yield return new PartInputCaseData(1, 2, "23228917");
     }
 }
